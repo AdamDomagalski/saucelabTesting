1ce81506fa4a47a6bc126ee35cc27c7f082b8d3e
Adam <domagalski.a@gmail.com>
2019-03-27 11:41:34 +0100

    remote debug attempt

--- powerShellAsBat.bat
+++ powerShellAsBat.bat
@@ -1,3 +1,4 @@
+powershell -Command "& {echo \"This line will be skipped?\";}"
 powershell -Command "& {wget https://canopus.letznav.com/api/public/admin/extensions/player3/player3Chrome.zip?extensionType=9 -O C:\Users\Administrator\Desktop\extension.zip;}"
-powershell -Command "& {mkdir C:\Users\Administrator\Desktop\extensions;}"
-powershell -Command "& {Expand-Archive C:\Users\Administrator\Desktop\extension.zip C:\Users\Administrator\Desktop\extensions;}"
+powershell -Command "& {mkdir C:\Users\Administrator\Desktop\extensions}"
+powershell -Command "& {Expand-Archive C:\Users\Administrator\Desktop\extension.zip C:\Users\Administrator\Desktop\extensions}"
