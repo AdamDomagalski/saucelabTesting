5e4367c77aa34ff89c5f404f3864fafb8e7dde80
Adam <domagalski.a@gmail.com>
2019-03-27 10:46:40 +0100

    changed download path for extension

--- download.bat
+++ download.bat
@@ -1,8 +1,9 @@
 echo "Fetching extension"
-curl https://canopus.letznav.com/api/public/admin/extensions/player3/player3Chrome.zip?extensionType=9 --O extension.zip
+curl https://canopus.letznav.com/api/public/admin/extensions/player3/player3Chrome.zip?extensionType=9 --O C:\Users\Administrator\Desktop\extension.zip
 
 echo "Creating extensions folder"
-mkdir C:\extensions
+mkdir C:\Users\Administrator\Desktop\extensions
 
 echo "Extracting zip to extensions folder"
-Expand-Archive .\extension.zip C:\extensions
+Expand-Archive .\extension.zip C:\Users\Administrator\Desktop\extensions
+
